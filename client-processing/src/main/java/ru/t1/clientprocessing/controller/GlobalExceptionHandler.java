01fc0ae818c064b51e0f78508275b2daa1552945
vegaklin <ruslan.evn@mail.ru>
2025-09-28 00:00:58 +0300

    feat: add ex hendler to ms1

--- client-processing/src/main/java/ru/t1/clientprocessing/controller/GlobalExceptionHandler.java
+++ client-processing/src/main/java/ru/t1/clientprocessing/controller/GlobalExceptionHandler.java
@@ -1,15 +1,28 @@
 package ru.t1.clientprocessing.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.t1.clientprocessing.dto.ClientErrorResponse;
+import ru.t1.clientprocessing.exception.*;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        return ResponseEntity.badRequest().body("error");
+    @ExceptionHandler({MethodArgumentNotValidException.class, NoCreditAmountException.class})
+    public ResponseEntity<ClientErrorResponse> handleBadRequestExceptions(RuntimeException ex) {
+        return ResponseEntity.badRequest().body(new ClientErrorResponse(ex.getMessage()));
+    }
+
+    @ExceptionHandler({NoClientException.class, NoClientProductException.class, NoProductException.class})
+    public ResponseEntity<ClientErrorResponse> handleNotFoundExceptions(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ClientErrorResponse(ex.getMessage()));
+    }
+
+    @ExceptionHandler(BlacklistedClientException.class)
+    public ResponseEntity<ClientErrorResponse> handleForbiddenExceptions(RuntimeException ex) {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(new ClientErrorResponse(ex.getMessage()));
     }
 }
